b887ae415938d00668e9902e46ca6fa64402f6d4
tszhong0411 <lainelson411@gmail.com>
2022-08-19 14:40:08 +0800

    :mag: 用 next seo 重寫 seo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,8 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
+import { NextSeo } from 'next-seo'
+import { useRouter } from 'next/router'
+
+import { Favicons } from '@/lib/Favicons'
 
 import Footer from '@/components/Footer'
 import useStyles from '@/components/index.styles'
@@ -8,12 +11,36 @@ import Profile from '@/components/Profile'
 
 const Home: NextPage = () => {
   const { classes } = useStyles()
+  const router = useRouter()
 
   return (
     <>
-      <Head>
-        <title>Link | 小康</title>
-      </Head>
+      <NextSeo
+        titleTemplate='%s | 小康'
+        defaultTitle='小康'
+        description='A teenager who want to become a frontend developer'
+        canonical={`https://link.honghong.me${router.asPath}`}
+        twitter={{
+          cardType: 'summary_large_image',
+          site: '@TszhongLai0411',
+          handle: '@TszhongLai0411',
+        }}
+        openGraph={{
+          url: `https://link.honghong.me${router.asPath}`,
+          type: 'website',
+          title: '小康',
+          description: 'A teenager who want to become a frontend developer',
+          images: [
+            {
+              url: 'https://honghong.me/static/images/banner.png',
+              width: 1920,
+              height: 929,
+              alt: 'A teenager who want to become a frontend developer',
+            },
+          ],
+        }}
+        additionalLinkTags={[...Favicons]}
+      />
       <div className={classes.wrapper}>
         <div className={classes.inner}>
           <div className={classes.bg}></div>
