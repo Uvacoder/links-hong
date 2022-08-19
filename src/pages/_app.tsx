682d45f17de9a014a1366e84895259a6501ad3e2
tszhong0411 <lainelson411@gmail.com>
2022-08-19 14:39:48 +0800

    :coffin: 移除沒有用的 code

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,4 @@
-import { ColorScheme, ColorSchemeProvider, Global, MantineProvider } from '@mantine/core'
-import { useHotkeys } from '@mantine/hooks'
-import { ModalsProvider } from '@mantine/modals'
-import { NotificationsProvider } from '@mantine/notifications'
-import { getCookie, setCookies } from 'cookies-next'
-import { GetServerSidePropsContext } from 'next'
+import { ColorScheme, Global, MantineProvider } from '@mantine/core'
 import type { AppProps } from 'next/app'
 import React from 'react'
 
@@ -11,81 +6,53 @@ import Layout from '@/components/Layout'
 
 export default function App(props: AppProps & { colorScheme: ColorScheme }) {
   const { Component, pageProps } = props
-  const [colorScheme, setColorScheme] = React.useState<ColorScheme>(props.colorScheme)
-
-  const toggleColorScheme = (value?: ColorScheme) => {
-    const nextColorScheme = value || (colorScheme === 'dark' ? 'light' : 'dark')
-    setColorScheme(nextColorScheme)
-    setCookies('mantine-color-scheme', nextColorScheme, {
-      maxAge: 60 * 60 * 24 * 30,
-    })
-  }
-
-  useHotkeys([['mod+J', () => toggleColorScheme()]])
 
   return (
-    <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
-      <MantineProvider
-        withGlobalStyles
-        withNormalizeCSS
-        theme={{
-          colorScheme,
-          fontFamily:
-            'Sora,Noto Sans TC,Inter,ui-sans-serif,system-ui,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Arial,Noto Sans,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol,Noto Color Emoji',
-          primaryColor: 'red',
-          breakpoints: {
-            xs: 375,
-            sm: 640,
-            md: 768,
-            lg: 1024,
-            xl: 1280,
+    <MantineProvider
+      withGlobalStyles
+      withNormalizeCSS
+      theme={{
+        fontFamily:
+          'Sora,Noto Sans TC,Inter,ui-sans-serif,system-ui,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Arial,Noto Sans,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol,Noto Color Emoji',
+        primaryColor: 'red',
+        breakpoints: {
+          xs: 375,
+          sm: 640,
+          md: 768,
+          lg: 1024,
+          xl: 1280,
+        },
+      }}
+    >
+      <Global
+        styles={() => ({
+          html: {
+            scrollBehavior: 'smooth',
           },
-        }}
-      >
-        <ModalsProvider>
-          <NotificationsProvider>
-            <Global
-              styles={() => ({
-                html: {
-                  scrollBehavior: 'smooth',
-                },
-                'ol, ul, menu': {
-                  listStyle: 'none',
-                  margin: 0,
-                  padding: 0,
-                },
-                '::selection': {
-                  background: 'rgb(249, 6, 6, 0.05)',
-                  color: '#f90606',
-                },
-                '::-webkit-scrollbar': {
-                  width: 7,
-                  height: 5,
-                },
-                '::-webkit-scrollbar-thumb': {
-                  background: '#ef4444',
-                  transition: '0.25s',
-                  borderRadius: 2,
-                },
-                '::-webkit-scrollbar-track': {
-                  background: '0 0',
-                },
-                // https://stackoverflow.com/questions/61083813/how-to-avoid-internal-autofill-selected-style-to-be-applied
-                'input:-webkit-autofill, input:-webkit-autofill:focus': {
-                  transition: 'background-color 600000s 0s, color 600000s 0s',
-                },
-              })}
-            />
-            <Layout>
-              <Component {...pageProps} />
-            </Layout>
-          </NotificationsProvider>
-        </ModalsProvider>
-      </MantineProvider>
-    </ColorSchemeProvider>
+          '::selection': {
+            background: 'rgb(249, 6, 6, 0.05)',
+            color: '#f90606',
+          },
+          '::-webkit-scrollbar': {
+            width: 7,
+            height: 5,
+          },
+          '::-webkit-scrollbar-thumb': {
+            background: '#ef4444',
+            transition: '0.25s',
+            borderRadius: 2,
+          },
+          '::-webkit-scrollbar-track': {
+            background: '0 0',
+          },
+          'input:-webkit-autofill, input:-webkit-autofill:focus': {
+            transition: 'background-color 600000s 0s, color 600000s 0s',
+          },
+        })}
+      />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </MantineProvider>
   )
 }
-
-App.getInitialProps = async ({ ctx }: { ctx: GetServerSidePropsContext }) => ({
-  colorScheme: getCookie('mantine-color-scheme', ctx) || 'light',
-})
